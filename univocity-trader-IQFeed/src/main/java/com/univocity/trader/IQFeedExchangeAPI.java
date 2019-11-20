2950665c7aabc98140df3cbc07218136ca5dd4e1
jecker7 <jhecker123@gmail.com>
2019-11-19 23:39:46 -0500

    updating IQFeedExchangeAPI

---
+++ univocity-trader-IQFeed/src/main/java/com/univocity/trader/IQFeedExchangeAPI.java
@@ -0,0 +1,93 @@
+package com.univocity.trader;
+
+import com.univocity.trader.candles.Candle;
+import com.univocity.trader.indicators.base.TimeInterval;
+import com.univocity.trader.vendor.iqfeed.api.client.impl.IQFeedAPIWebSocketClient;
+import com.univocity.trader.candles.SymbolInformation;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.util.HttpUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeoutException;
+
+public class IQFeedExchangeAPI implements ExchangeApi<Candlestick> {
+
+    private static final Logger log = LoggerFactory.getLogger(BinanceExchangeApi.class);
+
+    private IQFeedAPIWebSocketClient socketClient;
+    private org.asynchttpclient.ws.WebSocket socketClientCloseable;
+    private final Map<String, SymbolInformation> symbolInformation = new ConcurrentHashMap<>();
+
+    private final EventLoopGroup eventLoopGroup = new NioEventLoopGroup(2);
+    // TODO: ask about maxFrameSize
+    private final AsyncHttpClient asyncHttpClient = HttpUtils.newAsyncHttpClient(eventLoopGroup, 655356);
+
+    @Override
+    public IQFeedAccountApi connectToAccount(String api){
+        // TODO: implement this
+        return new IQFeedClientAccountApi();
+    }
+
+    @Override
+    public Candlestick getLatestTick(String symbol, TiemInterval interval){
+        // TODO: implement
+        List<Candlestick> candles = socketClient().getCandlestickBars(symbol, CandlestickInterval.fromTimeInterval(interval), 1, null, null);
+        if(candles != null && candles.size() > 0) {
+            return candles.get(0);
+        }
+        return null;
+    }
+
+    //TODO: implement
+    @Override
+    public List<Candlestick> getHistoricalTicks(String symbol, TimeInterval interval, long startTime, long endTime){
+
+    }
+    // TODO: add callback for connection login via IQFeed
+
+    @Override
+    public Candle generateCandle(Candlestick c) {
+        return new Candle(
+                c.getOpenTime(),
+                c.getCloseTime(),
+                Double.parseDouble(c.getOpen()),
+                Double.parseDouble(c.getHigh()),
+                Double.parseDouble(c.getLow()),
+                Double.parseDouble(c.getClose()),
+                Double.parseDouble(c.getVolume())
+        );
+    }
+
+//    @Override
+//    public List<Candlestick> getHistoricalTicks(String symbol, TimeInterval interval, long startTime, long endTime){
+//        return socketClient().getC
+//    }
+
+    @Override
+    public TimeInterval handleException(String action, String symbol, Exception e){
+        String message = "excecute " + action + " for " + symbol;
+        if(e.getCause() instanceof TimeoutException){
+            log.error("Timeout trying to ", message, e);
+        } else if (e.getCause() instanceof UnknownHostException) {
+            log.error("Unable to " + message + ". IQFeed is offline. ", e);
+            return TimeInterval.minutes(1);
+        } else {
+            log.error("Eror trying to " + message, e);
+        }
+    }
+
+    private IQFeedAPIWebSocketClient socketClient(){
+        if(socketClient == null){
+            IQFeedAPIClientFactory factory = IQFeedAPIClientFactory.newInstance(asyncHttpClient);
+            socketClient = factory.newWebSocketClient();
+        }
+        return socketClient;
+    }
+
+}
+
