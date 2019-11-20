87218d717b8f9bcb5cfc80d7f3b437e7d150c493
jecker <john.ecker@amadeus.com>
2019-11-20 17:08:54 -0500

    finishing exceptions class

--- univocity-trader-IQFeed/src/main/java/com/univocity/trader/vendor/iqfeed/api/client/exception/IQFeedApiException.java
+++ univocity-trader-IQFeed/src/main/java/com/univocity/trader/vendor/iqfeed/api/client/exception/IQFeedApiException.java
@@ -8,5 +8,21 @@ public class IQFeedApiException extends RuntimeException{
     public IQFeedApiException(IQFeedApiError error) { this.error = error;}
 
     public IQFeedApiException(){ super();}
+
+    public IQFeedApiException(String message){ super(message);}
+
+    public IQFeedApiException(Throwable cause){ super(cause);}
+
+    public IQFeedApiException(String message, Throwable cause) { super(message, cause);}
+
+    public IQFeedApiError getError() { return error;}
+
+    @Override
+    public String getMessage(){
+        if (error != null) {
+            return error.getMsg();
+        }
+        return super.getMessage();
+    }
 }
 
