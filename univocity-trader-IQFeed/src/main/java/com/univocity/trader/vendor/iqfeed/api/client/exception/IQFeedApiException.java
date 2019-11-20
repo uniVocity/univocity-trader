b99bc435022dfce349ecb6937bf1b75f3596fb97
jecker <john.ecker@amadeus.com>
2019-11-20 17:06:09 -0500

    adding in exception default constructors

--- univocity-trader-IQFeed/src/main/java/com/univocity/trader/vendor/iqfeed/api/client/exception/IQFeedApiException.java
+++ univocity-trader-IQFeed/src/main/java/com/univocity/trader/vendor/iqfeed/api/client/exception/IQFeedApiException.java
@@ -1,4 +1,12 @@
 package com.univocity.trader.vendor.iqfeed.api.client.exception;
 
+import com.univocity.trader.vendor.iqfeed.api.client.IQFeedApiError;
+
 public class IQFeedApiException extends RuntimeException{
+    private IQFeedApiError error;
+
+    public IQFeedApiException(IQFeedApiError error) { this.error = error;}
+
+    public IQFeedApiException(){ super();}
 }
+
