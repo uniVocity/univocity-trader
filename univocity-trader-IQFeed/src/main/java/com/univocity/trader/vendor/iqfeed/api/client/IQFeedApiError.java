d23416b4cc960cc753bea585dc7a47e2bfd8dca2
jecker <john.ecker@amadeus.com>
2019-11-20 17:11:39 -0500

    adding IQFeed Error class

--- univocity-trader-IQFeed/src/main/java/com/univocity/trader/vendor/iqfeed/api/client/IQFeedApiError.java
+++ univocity-trader-IQFeed/src/main/java/com/univocity/trader/vendor/iqfeed/api/client/IQFeedApiError.java
@@ -1,4 +1,16 @@
 package com.univocity.trader.vendor.iqfeed.api.client;
 
 public class IQFeedApiError {
+
+    private int code;
+    private String msg;
+
+    @Override
+    public String toString(){
+        return new ToStringBuilder(this, IQFeedApiConstants.TO_STRING_BUILDER_STYE)
+                .append("code", code)
+                .append("msg", msg)
+                .toString();
+    }
+
 }
