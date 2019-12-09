3af42d06ec3cf9223a1199dbba2f3bea4308f91f
Tom Everett <tom.everett@fglsports.com>
2019-12-09 17:16:48 -0500

    fun and games with univocity.properties

--- univocity-trader-core/src/main/java/com/univocity/trader/candles/CandleProcessor.java
+++ univocity-trader-core/src/main/java/com/univocity/trader/candles/CandleProcessor.java
@@ -1,56 +1,51 @@
 package com.univocity.trader.candles;
 
-import com.univocity.trader.*;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.univocity.trader.exchange.Exchange;
-import com.univocity.trader.strategy.*;
-import org.apache.commons.lang3.*;
-import org.slf4j.*;
+import com.univocity.trader.strategy.Engine;
 
 public class CandleProcessor<T> {
+   private static final Logger log = LoggerFactory.getLogger(CandleProcessor.class);
+   private final Engine consumer;
+   private final Exchange exchange;
 
-	private static final Logger log = LoggerFactory.getLogger(CandleProcessor.class);
+   public CandleProcessor(Engine consumer, Exchange<T> exchange) {
+      this.consumer = consumer;
+      this.exchange = exchange;
+   }
 
-	private final Engine consumer;
-	private final Exchange exchange;
+   public void processCandle(String symbol, Candle candle, boolean initializing) {
+      if (!StringUtils.equalsIgnoreCase(symbol, consumer.getSymbol())) {
+         return;
+      }
+      try {
+         if (!initializing) {
+            log.debug("{} - {}", symbol, candle);
+         }
+         consumer.process(candle, initializing);
+      } catch (Exception e) {
+         log.error("Error processing current candle:" + candle, e);
+      }
+   }
 
-	public CandleProcessor(Engine consumer, Exchange<T> exchange) {
-		this.consumer = consumer;
-		this.exchange = exchange;
-	}
-
-	public void processCandle(String symbol, Candle candle, boolean initializing) {
-		if (!StringUtils.equalsIgnoreCase(symbol, consumer.getSymbol())) {
-			return;
-		}
-		try {
-			if (!initializing) {
-				log.debug("{} - {}", symbol, candle);
-			}
-			consumer.process(candle, initializing);
-		} catch (Exception e) {
-			log.error("Error processing current candle:" + candle, e);
-		}
-	}
-
-	public void processCandle(String symbol, T realTimeTick, boolean initializing) {
-		try {
-			if (!StringUtils.equalsIgnoreCase(symbol, consumer.getSymbol())) {
-				return;
-			}
-			synchronized (consumer) {
-				PreciseCandle tick = exchange.generatePreciseCandle(realTimeTick);
-				if (!CandleRepository.addToHistory(consumer.getSymbol(), tick, initializing)) {  //already processed, skip.
-					return;
-				}
-				Candle candle = exchange.generateCandle(realTimeTick);
-
-				processCandle(symbol, candle, initializing);
-			}
-		} catch (Exception e) {
-			log.error("Error processing event:" + realTimeTick, e);
-		}
-	}
-
-
+   public void processCandle(String symbol, T realTimeTick, boolean initializing) {
+      try {
+         if (!StringUtils.equalsIgnoreCase(symbol, consumer.getSymbol())) {
+            return;
+         }
+         synchronized (consumer) {
+            PreciseCandle tick = exchange.generatePreciseCandle(realTimeTick);
+            if (!CandleRepository.addToHistory(consumer.getSymbol(), tick, initializing)) { // already processed, skip.
+               return;
+            }
+            Candle candle = exchange.generateCandle(realTimeTick);
+            processCandle(symbol, candle, initializing);
+         }
+      } catch (Exception e) {
+         log.error("Error processing event:" + realTimeTick, e);
+      }
+   }
 }
-
