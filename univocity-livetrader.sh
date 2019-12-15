145083e089ea841a9758960c971d6fa1a632257a
Tom Everett <tom.everett@fglsports.com>
2019-12-15 12:11:11 -0500

    refactoring again

--- univocity-livetrader.sh
+++ univocity-livetrader.sh
@@ -1,3 +1,3 @@
 #!\bin\sj
 
-java -jar univocity-trader-livetrader/target/univocity-trader-livetrader-1.0.0-SNAPSHOT.jar --config=univocity.properties --currencies="BTC"
+java -jar univocity-trader-cli/univocity-trader-livetrader/target/univocity-trader-livetrader-1.0.0-SNAPSHOT.jar --config=univocity.properties --currencies="BTC"
