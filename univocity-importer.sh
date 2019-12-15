c4736b73f08be73ece4c1b801643d8a28aa0ae6b
Tom Everett <tom.everett@fglsports.com>
2019-12-15 14:33:23 -0500

    bug fixes

--- univocity-importer.sh
+++ univocity-importer.sh
@@ -1,3 +1,3 @@
-#!\bin\sj
+#!\bin\sh
 
-java -jar univocity-trader-cli/univocity-trader-importer/target/univocity-trader-importer-1.0.0-SNAPSHOT.jar --config=univocity.properties --currencies="BTC"
+java -jar univocity-trader-cli/univocity-trader-importer/target/univocity-trader-importer-1.0.0-SNAPSHOT.jar --config=univocity.properties
--- univocity-simulator.sh
+++ univocity-simulator.sh
@@ -1,3 +1,3 @@
-#!\bin\sj
+#!\bin\sh
 
-java -jar univocity-trader-cli/univocity-trader-marketsimulator/target/univocity-trader-marketsimulator-1.0.0-SNAPSHOT.jar --config=univocity.properties --currencies="BTC"
+java -jar univocity-trader-cli/univocity-trader-marketsimulator/target/univocity-trader-marketsimulator-1.0.0-SNAPSHOT.jar --config=univocity.properties
