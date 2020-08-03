c9013884264dd0cf7a49faf056f5e1ea701150ad
Daniel Tavares <jdanielts1@gmail.com>
2020-08-03 15:18:15 -0300

    Implementing RAVIIndicator

---
+++ univocity-trader-core/src/main/java/com/univocity/trader/indicators/RAVIIndicator.java
@@ -0,0 +1,44 @@
+package com.univocity.trader.indicators;
+
+import com.univocity.trader.candles.Candle;
+import com.univocity.trader.indicators.base.SingleValueIndicator;
+import com.univocity.trader.indicators.base.TimeInterval;
+import com.univocity.trader.strategy.Indicator;
+
+public class RAVIIndicator extends SingleValueIndicator {
+
+    private MovingAverage shortSma;
+    private MovingAverage longSma;
+
+    private double value;
+
+    public RAVIIndicator(int shortSmaBarCount, int longSmaBarCount, TimeInterval timeInterval) {
+        super(timeInterval, null);
+        shortSma = new MovingAverage(shortSmaBarCount, timeInterval);
+        longSma = new MovingAverage(longSmaBarCount, timeInterval);
+    }
+
+    @Override
+    protected boolean process(Candle candle, double value, boolean updating) {
+        shortSma.calculateIndicatorValue(candle, value, updating);
+        double shortMA = shortSma.getValue();
+
+        longSma.calculateIndicatorValue(candle, value, updating);
+        double longMA = longSma.getValue();
+
+        this.value = ((shortMA - longMA) / longMA) * 100;
+
+        return true;
+    }
+
+    @Override
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    protected Indicator[] children() {
+        return new Indicator[]{};
+    }
+
+}
