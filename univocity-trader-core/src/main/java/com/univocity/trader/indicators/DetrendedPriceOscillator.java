8a6625d91447a6bde34e767333b5992a38d23bad
Jeronimo Backes <jbax@univocity.com>
2020-08-10 19:28:50 +0930

    Fixed DPOIndicator calculation

---
+++ univocity-trader-core/src/main/java/com/univocity/trader/indicators/DetrendedPriceOscillator.java
@@ -0,0 +1,52 @@
+package com.univocity.trader.indicators;
+
+import com.univocity.trader.candles.*;
+import com.univocity.trader.indicators.base.*;
+import com.univocity.trader.strategy.*;
+import com.univocity.trader.utils.*;
+
+import java.util.function.*;
+
+public class DetrendedPriceOscillator extends SingleValueIndicator {
+
+	private double value;
+	private MovingAverage ma;
+	private CircularList timeShift;
+
+	public DetrendedPriceOscillator(TimeInterval interval) {
+		this(9, interval);
+	}
+
+	public DetrendedPriceOscillator(int length, TimeInterval interval) {
+		this(length, interval, c -> c.close);
+	}
+
+	public DetrendedPriceOscillator(int length, TimeInterval interval, ToDoubleFunction<Candle> valueGetter) {
+		super(interval, valueGetter);
+		ma = new MovingAverage(length, interval, valueGetter);
+		timeShift = new CircularList((length / 2) + 1);
+	}
+
+
+	@Override
+	protected boolean process(Candle candle, double value, boolean updating) {
+		if (ma.accumulate(candle)) {
+			timeShift.accumulate(ma.getValue(), updating);
+			this.value = candle.close - timeShift.first();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public double getValue() {
+		return value;
+	}
+
+	@Override
+	protected Indicator[] children() {
+		return new Indicator[]{ma};
+	}
+
+}
--- univocity-trader-core/src/test/java/com/univocity/trader/indicators/DetrendedPriceOscillatorTest.java
+++ univocity-trader-core/src/test/java/com/univocity/trader/indicators/DetrendedPriceOscillatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class DPOIndicatorTest {
+public class DetrendedPriceOscillatorTest {
 
     private static final double[] values = {22.27, 22.19, 22.08, 22.17, 22.18, 22.13, 22.23, 22.43, 22.24, 22.29,
             22.15, 22.39, 22.38, 22.61, 23.36, 24.05, 23.75, 23.83, 23.95, 23.63, 23.82, 23.87, 23.65, 23.19, 23.10,
@@ -21,7 +21,7 @@ public class DPOIndicatorTest {
 
         int i = 0;
 
-        DPOIndicator indicator = new DPOIndicator(9, TimeInterval.minutes(1));
+        DetrendedPriceOscillator indicator = new DetrendedPriceOscillator(9, TimeInterval.minutes(1));
 
         indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
         indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
@@ -33,16 +33,16 @@ public class DPOIndicatorTest {
         indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
         indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
         indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
-        assertEquals(0.111999, indicator.getValue(), 0.0001);
+        assertEquals(0.12, indicator.getValue(), 0.0001);
 
         indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
-        assertEquals(-0.02, indicator.getValue(), 0.0001);
+        assertEquals(-0.028571428571424917, indicator.getValue(), 0.0001);
 
         indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
-        assertEquals(0.21142857142, indicator.getValue(), 0.0001);
+        assertEquals(0.18000000000000327, indicator.getValue(), 0.0001);
 
         indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
-        assertEquals(0.169999999999999, indicator.getValue(), 0.0001);
+        assertEquals(0.16666666666666785, indicator.getValue(), 0.0001);
     }
 
 }
