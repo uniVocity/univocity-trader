0de96010e5d25f453d0885c1f06f9eeff62db70a
Jeronimo Backes <jbax@univocity.com>
2020-08-18 17:25:03 +0930

    Adding CommodityChannelIndex

---
+++ univocity-trader-core/src/test/java/com/univocity/trader/indicators/CommodityChannelIndexTest.java
@@ -0,0 +1,47 @@
+package com.univocity.trader.indicators;
+
+import com.univocity.trader.candles.CandleHelper;
+import com.univocity.trader.indicators.base.TimeInterval;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CommodityChannelIndexTest {
+
+    private double[] values = new double[] { 23.98, 23.92, 23.79, 23.67, 23.54, 23.36, 23.65, 23.72, 24.16,
+            23.91, 23.81, 23.92, 23.74, 24.68, 24.94, 24.93, 25.10, 25.12, 25.20, 25.06, 24.50, 24.31, 24.57, 24.62,
+            24.49, 24.37, 24.41, 24.35, 23.75, 24.09 };
+
+    @Test
+    public void cci() {
+
+        int i = 0;
+
+        CommodityChannelIndex indicator = new CommodityChannelIndex(20, TimeInterval.MINUTE);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(-66.6667, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(-100d, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(14.365, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(54.2544, indicator.getValue(), 0.0001);
+
+
+    }
+
+}
