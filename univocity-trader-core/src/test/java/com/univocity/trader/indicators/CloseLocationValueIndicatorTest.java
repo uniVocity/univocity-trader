87fe90d579efc3dd6e296646283e3b9561053c02
Daniel Tavares <jdanielts1@gmail.com>
2020-08-18 23:02:05 -0300

    creating CloseLocationValueIndicator (#80)

---
+++ univocity-trader-core/src/test/java/com/univocity/trader/indicators/CloseLocationValueIndicatorTest.java
@@ -0,0 +1,43 @@
+package com.univocity.trader.indicators;
+
+import com.univocity.trader.candles.CandleHelper;
+import com.univocity.trader.indicators.base.TimeInterval;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CloseLocationValueIndicatorTest {
+
+    @Test
+    public void closeLocation() {
+
+        int i = 0;
+
+        CloseLocationValueIndicator indicator = new CloseLocationValueIndicator(TimeInterval.MINUTE);
+        indicator.accumulate(CandleHelper.newCandle(i++, 10, 18, 20, 10));
+        assertEquals(0.6, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 17, 20, 21, 17));
+        assertEquals(0.5, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 15, 15, 16, 14));
+        assertEquals(0, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 15, 11, 15, 8));
+        assertEquals(-1d / 7, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 11, 12, 12, 10));
+        assertEquals(1, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 10, 10, 10, 10));
+        assertEquals(0, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 11, 12, 12, 10));
+        assertEquals(1, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 11, 120, 140, 100));
+        assertEquals(0, indicator.getValue(), 0.0001);
+
+    }
+
+}
