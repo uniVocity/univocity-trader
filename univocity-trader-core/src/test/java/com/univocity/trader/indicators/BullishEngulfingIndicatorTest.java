7f425d7b4f4884e5a5f8002f336accc3acc7451e
Daniel Tavares <jdanielts1@gmail.com>
2020-08-23 11:03:53 -0300

    creating BullishEngulfingIndicator

---
+++ univocity-trader-core/src/test/java/com/univocity/trader/indicators/BullishEngulfingIndicatorTest.java
@@ -0,0 +1,35 @@
+package com.univocity.trader.indicators;
+
+import com.univocity.trader.candles.CandleHelper;
+import com.univocity.trader.indicators.base.TimeInterval;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BullishEngulfingIndicatorTest {
+
+    @Test
+    public void bullishEngulfingIndicator() {
+
+        int i = 0;
+
+        BullishEngulfingIndicator indicator = new BullishEngulfingIndicator(TimeInterval.MINUTE);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 10, 18, 20, 10));
+        assertEquals(0, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 17, 16, 19, 15));
+        assertEquals(0, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 15, 18, 19, 14));
+        assertEquals(1, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 15, 11, 15, 8));
+        assertEquals(0, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i++, 11, 12, 12, 10));
+        assertEquals(0, indicator.getValue(), 0.0001);
+
+    }
+
+}
