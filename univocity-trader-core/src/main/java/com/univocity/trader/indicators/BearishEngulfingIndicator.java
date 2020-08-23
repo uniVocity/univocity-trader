9729a5972b6d14004c91f784dfed29d3a32d15c9
Daniel Tavares <jdanielts1@gmail.com>
2020-08-23 00:20:47 -0300

    creating BearishEngulfingIndicator

---
+++ univocity-trader-core/src/main/java/com/univocity/trader/indicators/BearishEngulfingIndicator.java
@@ -0,0 +1,55 @@
+package com.univocity.trader.indicators;
+
+import com.univocity.trader.candles.Candle;
+import com.univocity.trader.indicators.base.SingleValueIndicator;
+import com.univocity.trader.indicators.base.TimeInterval;
+import com.univocity.trader.strategy.Indicator;
+
+public class BearishEngulfingIndicator extends SingleValueIndicator {
+
+    private double value;
+    private Candle prev;
+
+    public BearishEngulfingIndicator(TimeInterval interval) {
+        super(interval, null);
+    }
+
+    @Override
+    protected boolean process(Candle candle, double value, boolean updating) {
+        boolean toReturn = false;
+        this.value = 0;
+
+        if(prev == null) {
+
+            toReturn = true;
+
+        } else if (prev.isClosePositive() && !candle.isClosePositive()) {
+
+            final double prevOpenPrice = prev.open;
+            final double prevClosePrice = prev.close;
+            final double currOpenPrice = candle.open;
+            final double currClosePrice = candle.close;
+
+            this.value = (currOpenPrice > prevOpenPrice && currOpenPrice > prevClosePrice
+                    && currClosePrice < prevOpenPrice && currClosePrice < prevClosePrice) ? 1 : 0;
+
+            toReturn = true;
+
+        }
+
+        prev = candle;
+
+        return toReturn;
+    }
+
+    @Override
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    protected Indicator[] children() {
+        return new Indicator[]{};
+    }
+
+}
