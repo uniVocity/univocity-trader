7cd4f349ec5468e80efbd29ced87a4dcce04b247
Daniel Tavares <jdanielts1@gmail.com>
2020-08-24 04:24:03 -0300

    DojiIndicator (#92)
    * creating DojiIndicator
    
    * fixing probleam in DojiIndicator

---
+++ univocity-trader-core/src/main/java/com/univocity/trader/indicators/DojiIndicator.java
@@ -0,0 +1,62 @@
+package com.univocity.trader.indicators;
+
+import com.univocity.trader.candles.Candle;
+import com.univocity.trader.indicators.base.MultiValueIndicator;
+import com.univocity.trader.indicators.base.TimeInterval;
+import com.univocity.trader.strategy.Indicator;
+
+public class DojiIndicator extends MultiValueIndicator {
+
+    private double value;
+    private double bodyFactor;
+    private double prevAverageBodyHeightInd;
+
+    private final RealBodyIndicator bodyHeightInd;
+    private final MovingAverage averageBodyHeightInd;
+
+    public DojiIndicator(TimeInterval interval) {
+        this(0.03, 10, interval);
+    }
+
+    public DojiIndicator(double bodyFactor, int length, TimeInterval interval) {
+        super(length, interval, null);
+        this.bodyHeightInd = new RealBodyIndicator(interval);
+        this.averageBodyHeightInd = new MovingAverage(length, interval, c -> Math.abs(bodyHeightInd.getValue()));
+        this.bodyFactor = bodyFactor;
+        this.prevAverageBodyHeightInd = 0;
+    }
+
+    @Override
+    protected boolean calculateIndicatorValue(Candle candle, double value, boolean updating) {
+
+        if (this.bodyHeightInd.accumulate(candle)) {
+            this.averageBodyHeightInd.accumulate(candle);
+
+            if (values.size() == 1) {
+                this.value = Math.abs(bodyHeightInd.getValue()) == 0 ? 1 : 0;
+                return true;
+            }
+
+            double averageBodyHeight = prevAverageBodyHeightInd;
+            double currentBodyHeight = Math.abs(bodyHeightInd.getValue());
+
+            this.value = currentBodyHeight < (averageBodyHeight * this.bodyFactor) ? 1 : 0;
+            this.prevAverageBodyHeightInd = averageBodyHeightInd.getValue();
+
+            return true;
+
+        }
+
+        return false;
+    }
+
+    @Override
+    public double getValue() {
+        return this.value;
+    }
+
+    @Override
+    protected Indicator[] children() {
+        return new Indicator[]{bodyHeightInd, averageBodyHeightInd};
+    }
+}
