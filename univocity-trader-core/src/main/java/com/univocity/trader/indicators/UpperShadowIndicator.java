d09f3f92a0c244bb948d533ff229d3f28297afec
Daniel Tavares <jdanielts1@gmail.com>
2020-08-24 04:21:01 -0300

    creating UpperShadowIndicator (#90)

---
+++ univocity-trader-core/src/main/java/com/univocity/trader/indicators/UpperShadowIndicator.java
@@ -0,0 +1,24 @@
+package com.univocity.trader.indicators;
+
+import com.univocity.trader.candles.Candle;
+import com.univocity.trader.indicators.base.SingleValueCalculationIndicator;
+import com.univocity.trader.indicators.base.TimeInterval;
+import com.univocity.trader.strategy.Indicator;
+
+public class UpperShadowIndicator extends SingleValueCalculationIndicator {
+
+    public UpperShadowIndicator(TimeInterval interval) {
+        super(interval);
+    }
+
+    @Override
+    protected double calculate(Candle candle, double value, double previousValue, boolean updating) {
+        return candle.close > candle.open ? candle.high - candle.close : candle.high - candle.open;
+    }
+
+    @Override
+    protected Indicator[] children() {
+        return new Indicator[]{};
+    }
+
+}
