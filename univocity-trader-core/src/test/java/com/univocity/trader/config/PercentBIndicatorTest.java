a45c30f51fab0aa0e8274241396bd4f994679ffa
Daniel Tavares <jdanielts1@gmail.com>
2020-08-26 03:01:36 -0300

    creating PercentBIndicator (#101)

---
+++ univocity-trader-core/src/test/java/com/univocity/trader/config/PercentBIndicatorTest.java
@@ -0,0 +1,84 @@
+package com.univocity.trader.config;
+
+import com.univocity.trader.candles.CandleHelper;
+import com.univocity.trader.indicators.PercentBIndicator;
+import com.univocity.trader.indicators.base.TimeInterval;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PercentBIndicatorTest {
+
+
+    private double[] values = {10, 12, 15, 14, 17, 20, 21, 20, 20, 19, 20, 17, 12, 12, 9, 8, 9, 10, 9, 10};
+
+    @Test
+    public void percentBUsingSMAAndStandardDeviation() {
+
+        int i = 0;
+
+        PercentBIndicator indicator = new PercentBIndicator(5, 2, TimeInterval.MINUTE);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.75, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.8244, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.6627, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.8517, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.90328, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.83, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.6552, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.5737, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.1047, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.5, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.0284, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.0344, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.2064, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.1835, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.2131, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.3506, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.5737, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.5, indicator.getValue(), 0.0001);
+
+        indicator.accumulate(CandleHelper.newCandle(i, values[i++]));
+        assertEquals(0.7673, indicator.getValue(), 0.0001);
+
+    }
+
+}
